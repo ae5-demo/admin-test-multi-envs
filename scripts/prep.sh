78b265c45201fca9e57fc811e8f67b232d920216
admin <admin@anaconda.com>
2020-03-05 22:33:25 +0000

    1

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,9 +1,11 @@
 # update anaconda-project and prepare all envs
 conda update anaconda-project -y && anaconda-project prepare --all
+conda init
+source ~/.bashrc
 conda activate Julia
 
 # install ijulia kernel - requires internet access..
 julia scripts/ijulia.jl
 
 # install kernel in local jupyter
-jupyter kernelspec install julia-1.1 --prefix $CONDA_PREFIX
+jupyter kernelspec install /opt/continuum/.local/share/jupyter/kernels/julia-1.1 --prefix $CONDA_PREFIX
